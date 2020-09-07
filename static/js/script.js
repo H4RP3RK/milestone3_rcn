8c334f847e54e22b06180f9def4fc5a1fc619b67
H4RP3RK <kirsty.harper@zoho.com>
2020-09-07 11:40:09 +0000

    member_home - added scroll to questions js function

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,11 @@
-
+/*-question_details.html - btn scrolls to Contacts section-*/
 function scrollContacts() {
     let elmnt = document.getElementById("contacts");
+    elmnt.scrollIntoView();
+}
+
+/*-member_home.html - btn scrolls to Questions section-*/
+function scrollQuestions() {
+    let elmnt = document.getElementById("questions");
     elmnt.scrollIntoView();
 }
