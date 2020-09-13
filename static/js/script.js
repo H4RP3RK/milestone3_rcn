7119e7de665c0937641e02e407279dff1ed85280
H4RP3RK <kirsty.harper@zoho.com>
2020-09-13 17:53:15 +0000

    member_list - added search bar

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,21 @@
 /*-question_details.html - btn scrolls to Contacts section-*/
 function scrollContacts() {
-    let elmnt = document.getElementById("contacts");
+    let elmnt = document.querySelector("#contacts");
     elmnt.scrollIntoView();
     elmnt.classList.toggle("border-glow");
 }
 
 /*-member_home.html - btn scrolls to Questions section-*/
 function scrollQuestions() {
-    let elmnt = document.getElementById("questions");
+    let elmnt = document.querySelector("#questions");
     elmnt.scrollIntoView();
     elmnt.classList.toggle("border-glow");
+}
+
+/*-member_list.html - search through members-*/
+function search() {
+    const member_names = document.querySelectorAll("h5");
+    const inputName = document.querySelector("#member_name").nodeValue.toLowerCase();
+
+    
 }
