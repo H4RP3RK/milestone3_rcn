c2f32d03f82181a6d31a96128546504bfc660a15
H4RP3RK <kirsty.harper@zoho.com>
2020-09-19 13:20:41 +0000

    shared_login - added top/bottom padding

--- app.py
+++ app.py
@@ -403,5 +403,5 @@ def member_details(member_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=False)
+            debug=True)
 ß
